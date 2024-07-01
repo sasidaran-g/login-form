bc5fb2626845f0a310101e5f2d2717e6ab78bd8a
sasidarang23 <sasidaran.gnanakumar@gmail.com>
2024-07-01 12:42:29 +0530

    changes in login

--- UI/src/app/apiservice/apiservice.service.ts
+++ UI/src/app/apiservice/apiservice.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AuthConfig,OAuthService } from 'angular-oauth2-oidc';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiserviceService {
-  constructor(private oauthservice:OAuthService,private http:HttpClient){}
+  constructor(private oauthservice: OAuthService, private http: HttpClient) {}
 
-  initAuth(){
+  initAuth() {
     this.oauthservice.configure({
-      issuer:'',
-      clientId:'518727039166-1v83hb42f5lrqoiskspine75h54beh4m.apps.googleusercontent.com',
-      redirectUri:window.location.origin,
-      responseType:'code',
-      scope:'sasidaran.gnanakumar@gmail.com'
+      issuer: '',
+      clientId:
+        '518727039166-1v83hb42f5lrqoiskspine75h54beh4m.apps.googleusercontent.com',
+      redirectUri: window.location.origin,
+      responseType: 'code',
+      scope: 'sasidaran.gnanakumar@gmail.com',
     });
     this.oauthservice.loadDiscoveryDocumentAndTryLogin();
   }
-  login(){
-    return this.oauthservice.fetchTokenUsingPasswordFlow(username,password);
+  login(email: string, password: string) {
+    return this.oauthservice.fetchTokenUsingPasswordFlow(email, password);
   }
 }
