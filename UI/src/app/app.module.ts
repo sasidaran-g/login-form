1f75e4193b507a0be5042f7b884a9cd5f1bb452a
sasidarang23 <sasidaran.gnanakumar@gmail.com>
2024-07-01 11:51:15 +0530

    some changes

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FirstpageComponent } from './firstpage/firstpage.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginFormComponent, SignupFormComponent],
+  declarations: [AppComponent, LoginFormComponent, SignupFormComponent, FirstpageComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
